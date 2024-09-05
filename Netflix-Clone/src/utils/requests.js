38b0defe80e9c8a6ea31a1f9ccb0e00e4fb10fd0
AbeloByte <gobeztech20@gmail.com>
2024-09-05 00:07:57 -0400

    MODIFIED envireonment Variable

--- Netflix-Clone/src/utils/requests.js
+++ Netflix-Clone/src/utils/requests.js
@@ -1,4 +1,4 @@
-const Api_key = import.meta.env.REACT_APP_API_Key
+const Api_key = import.meta.env.VITE_API_KEY
 
 
 
@@ -7,7 +7,6 @@ const requests = {
     fetchTrending: `/trending/all/week?api_key=${Api_key}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${Api_key}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${Api_key}&language=en-US`,
-
     fetchActionMovies: `/discover/movie?api_key=${Api_key}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${Api_key}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${Api_key}&with_genres=27`,
@@ -15,6 +14,7 @@ const requests = {
     fetchDocumentaries: `/discover/movie?api_key=${Api_key}&with_genres=99`,
     fetchAnimation : `/discover/movie?api_key=${Api_key}&with_genres=16`,
     fetchTvShow: `tv/popular?api_key=${Api_key}&language=en-US&page=1`
+
     
 
 }
