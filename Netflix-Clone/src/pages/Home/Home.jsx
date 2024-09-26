c300952a2162a9c70a2186f300b5533ae1f7ffb4
AbeloByte <gobeztech20@gmail.com>
2024-09-26 09:40:14 -0400

    Home

--- Netflix-Clone/src/pages/Home/Home.jsx
+++ Netflix-Clone/src/pages/Home/Home.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banner/Banner'
+// import Row from '../../components/Row/Row/Row'
+import RowList from '../../components/Row/RowList/RowList'
 
 function Home() {
   return (
     <>    
     <Header/>
     <Banner/>
+    <RowList/>
     <Footer/>
     </>
 
